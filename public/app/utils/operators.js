36b47f64b593b323b4ce5d0d6953127721ae94e5
Eduardo Costa01 <eccosta2000@bol.com.br>
2023-03-06 17:53:30 -0300

    JS-Abordagem-Funcional

---
+++ public/app/utils/operators.js
@@ -0,0 +1,24 @@
+export const partialize = (fn, ...args) =>
+    fn.bind(null, ...args);
+
+export const compose = (...fns) => value => 
+    fns.reduceRight((previousValues, fn) => 
+        fn(previousValues), value);
+
+export const pipe = (...fns) => value => 
+    fns.reduce((previousValues, fn) => 
+        fn(previousValues), value);
+
+
+export const takeUntil = (times, fn) =>
+    () => times-- > 0 && fn();
+
+
+export const debounceTime =  (milliseconds, fn) => {
+    let timer = 0
+    return () => {
+        clearTimeout(timer)
+        timer = setTimeout(fn, milliseconds);
+    };
+};
+
